1d2e2b950fd0d52c1a672fcae1a0731b2f2285e2
Rotich2000 <drkimutai06@gmail.com>
2022-12-12 17:18:23 +0300

    fix an image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
+import Logo from "../public/assets/logo.png"
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsWhatsapp } from "react-icons/bs";
@@ -49,7 +50,7 @@ const Navbar = () => {
     <div style={{backgroundColor: `${navBg}`}} className={shadow ? "fixed w-full h-20 shadow-xl z-[100]" : "fixed w-full h-20 z-[100]"}>
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
         <Image
-          src="/../public/assets/logo.png"
+          src={Logo}
           alt="logo"
           width="125"
           height="50"
